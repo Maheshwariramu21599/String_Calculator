7c0d4db355501f368e6b88b9c3ea7502421249d1
Mahi <mahiramu21599@gmail.com>
2024-05-02 11:42:27 +0530

    Added logic to handle negative values

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,13 @@ class StringCalculator
     
         nums = numbers.split(/[\n,]/).map(&:to_i)
     
+        negatives = nums.select(&:negative?)
+        if negatives.any?
+            raise "negatives not allowed: #{negatives.join(', ')}"
+        end
+
         nums.reduce(0, :+)
+    
     end
     
 end
